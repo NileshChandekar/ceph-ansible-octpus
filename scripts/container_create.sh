52b6adf502b08d286d767bd4a5160eb4dabb72aa
NileshChandekar <35838968+NileshChandekar@users.noreply.github.com>
2022-11-09 19:00:28 +0530

    Update container_create.sh

--- scripts/container_create.sh
+++ scripts/container_create.sh
@@ -18,12 +18,12 @@ nileshc85/ceph-ansible-2.9-octopus:20.04
 
 echo -e "\033[1;36mCopy IP and HOST Files in Container\033[0m"
 
-for spawn in $( sudo docker ps | grep -i $user | grep -i deploy | awk {'print $1'} ) ; \
+for spawn in $( sudo docker ps | grep -i $user | grep -i ceph | awk {'print $1'} ) ; \
 do sudo docker cp /tmp/$user/cephtxtfiles/test4.txt $spawn:/ip.txt ; done
 
-for spawn in $( sudo docker ps | grep -i $user | grep -i deploy | awk {'print $1'} ) ; \
+for spawn in $( sudo docker ps | grep -i $user | grep -i ceph | awk {'print $1'} ) ; \
 do sudo docker cp /tmp/$user/cephtxtfiles/test2.txt $spawn:/hostentry.txt ; done
 
 echo -e "\033[1;36mGet Inside of Container\033[0m"
-for spawn in $( sudo docker ps | grep -i $user | grep -i deploy | awk {'print $1'} ) ; \
+for spawn in $( sudo docker ps | grep -i $user | grep -i ceph | awk {'print $1'} ) ; \
 do sudo docker exec -it -u root $spawn bash ; done
